3a243743ebb9220e3ae93085b29c7d319d9da3b5
zhangjie1230 <172234298@qq.com>
2018-05-25 10:02:46 +0800

    首次提交

---
+++ src/main/java/com/example/demo/Helloboot.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class Helloboot {
+    @RequestMapping("/hello")
+    public String getString(){
+        return "hello boot";
+    }
+}
